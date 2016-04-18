9bfeaaa5aaf165defe4df0fd3803b7a06858f012
my-spock <rplowman15@gmail.com>
2016-04-18 15:18:11 -0600

    push

--- app.js
+++ app.js
@@ -7,45 +7,49 @@ function getMusic(){
 
 //My Code
 function drawSongs(songList) {
-  
-  for (var i = 0; i < 50; i++) {
-    var currentSong = songList[i];
     var ul = document.getElementById("search-results");
+    ul.innerHTML = "";
+  
+  for (var i = 0; i < songList.length; i++) {
+    var currentSong = songList[i];
     var li = document.createElement("LI");
     
     li.className = "row";
     
     li.innerHTML = 
       // img col
-      "<span class='col-xs-1' id='album-art'>" +
-      "<img src='" + currentSong.albumArt + "' width='50'> " +
+      "<span class='col-xs-2 col-md-1 col-md-offset-1 album-art'>" +
+      "<img src='" + currentSong.albumArt + "'> " +
       "</span>" +
-      // title row
-      "<div class='col-xs-8'>" +
+       //title&album row
+      "<div class='col-xs-3'>" +
       "<div class='row'>" +
-      "<div class='col-xs-1'>" +
-      "<a class='glyphicon glyphicon-play-circle' href='" + currentSong.preview + "'> </a>" + 
-      "</div>" +
-      "<div class='col-xs-9 text-left' id='title'>" +
-      currentSong.title +
+      "<div class='col-xs-12 text-left title'>" +
+      "<strong>" + currentSong.title + "</strong>" +
       "</div>" +
       "</div>" /*end title row*/ + 
       // album row
       "<div class='row'>" +
-      "<div class='col-xs-12'" +
-      "<p id='album'> Album: " + currentSong.collection + "</p>" +
+      "<div class='col-xs-12 album'>" +
+      "Album: " + currentSong.collection +
       "</div>" +
       "</div>" /*end album row*/ +
       // artist row
       "<div class='row'>" +
-      "<div class='col-xs-12'>" +
-      "<p id='artist'> By: " + currentSong.artist + "</p>" +
+      "<div class='col-xs-12 artist'>" +
+      "By: " + currentSong.artist +
       "</div>" +
       "</div>" /*end artist row*/ +
       "</div>" /*end col-xs-8*/ +
+      //preview col
+      "<div class='col-xs-6 preview'>" +
+      "<audio controls>" +
+      "<source src='" + currentSong.preview + "'>" +
+      "</audio>" +
+      "</div>" +
       // price col
-      "<div class='col-xs-2 col-xs-pull-right'>" +
-      "<p id='price'> Price: $" + currentSong.price + "</p>" +
+      "<div class='col-xs-1 col-xs-pull-right price'>" +
+      "Price: $" + currentSong.price +
       "</div>";
 
     ul.appendChild(li);
